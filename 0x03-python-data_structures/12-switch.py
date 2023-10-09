b04647f9945acb12a4d9c3f74b1c400ed95cc1f1
Alphaeu <dealphaeus@gmail.com>
2023-10-09 12:58:23 -0700

    Task Corrections

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 a = 89
 b = 10
-
-"""Switching values without using a temporary variable."""
 a, b = b, a
 print("a={:d} - b={:d}".format(a, b))
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    """Remove all characters c and C from a string."""
-    filtered_string = []
+    updated_str = ''
     for i in my_string:
         if i != 'c' and i != 'C':
-            filtered_string.append(i)
-    return " ".join(filtered_string)
+            updated_str += i
+    return (updated_str)
