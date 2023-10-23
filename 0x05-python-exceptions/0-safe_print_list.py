ef8fb431bccd580c976cff76c83af654b783a440
Alphaeu <dealphaeus@gmail.com>
2023-10-23 13:04:38 -0700

    Errors & Exceptions Handlers

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,12 +10,12 @@ def safe_print_list(my_list=[], x=0):
     Returns:
         The number of elements printed.
     """
-    ret = 0
+    total = 0
     for i in range(x):
         try:
             print(f"{my_list[i]}", end="")
-            ret += 1
+            total += 1
         except IndexError:
             break
-    print("")
-    return(ret)
+    print()
+    return(total)
