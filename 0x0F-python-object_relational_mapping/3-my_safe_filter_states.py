5049d155a586a3716bddf4d12be8f7978f352517
Alphaeu <dealphaeus@gmail.com>
2024-01-15 03:18:56 -0800

    Task updating

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,30 +1,15 @@
 #!/usr/bin/python3
-
 """
-This script maps an argument and displays 
-all values in the states where `name` matches 
-the argument from the database `hbtn_0e_0_usa`.
-This time the script is safe from
-MySQL injections!
+Python script that uses MySQLdb to display all values 
+matching a given name in the states table of the database hbtn_0e_0_usa
+Results are ordered by ascending states.id.
+Safe from SQL injections
 """
-
-import MySQLdb as db
-from sys import argv
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
-
-    db_cursor = db_connect.cursor()
-    db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE \
-                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
-
-    rows_selected = db_cursor.fetchall()
-
-    for row in rows_selected:
-        print(row)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
